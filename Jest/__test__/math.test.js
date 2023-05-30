4aafba5a8fdd589ba2f908c62c72434eec0dce14
hachecom <antocomisso@gmail.com>
2023-05-30 03:57:17 -0300

    configuración de entorno para ejercicios de Jest

---
+++ Jest/__test__/math.test.js
@@ -0,0 +1,16 @@
+const { sum, sub, mul, div } = require('../src/math.js');
+describe('juego de operaciones aritmeticas', () => {
+    test('suma de numeros', () => {
+        expect(sum(1, 1)).toBe(2);
+    });
+    test('resta de numeros', () => {
+        expect(sub(1, 1)).toBe(0);
+    });
+    test('multiplicacion de numeros', () => {
+        expect(mul(1, 1)).toBe(1);
+    });
+    test('division de numeros', () => {
+        expect(div(1, 1)).toBe(1);
+    });
+});
+405
