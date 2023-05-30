0c326da74cb22d6b68feb3b79cd4c461233d5aa4
hachecom <antocomisso@gmail.com>
2023-05-30 17:00:10 -0300

    Ej 1.1 de jest

---
+++ Jest/src/Ejercicio_11.js
@@ -0,0 +1,21 @@
+function fizzBuzz(valor) {
+    if (typeof (valor) !== "number") {
+        return ("Debe ingresar un número");
+    }
+    if (valor % 3 == 0 & valor % 5 == 0) {
+        return ("FizzBuzz")
+    }; // si el número es divisible por 3 y 5, devuelve FizzBuzz
+    if (valor % 3 == 0) {
+        return ("Fizz"); // si el número es divisible por 3, devuelve Fizz
+    }
+    if (valor % 5 == 0) {
+        return ("Buzz"); // si el número es divisible por 5, devuelve Buzz
+    }
+
+    else {
+        return (valor) // si el número no cumple ninguna de las condiciones anteriores, devuelve el número
+    }
+}
+
+module.exports = { fizzBuzz };
+console.log(fizzBuzz(15))
