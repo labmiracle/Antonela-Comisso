360d8ccdb457b905ee16afe43139d33c6223d337
hachecom <antocomisso@gmail.com>
2023-05-30 01:20:37 -0300

    ejercicio 2.4 y configuracion del formatter

--- Typescript_project/src/ejercicio_21.ts
+++ Typescript_project/src/ejercicio_21.ts
@@ -1,18 +1,18 @@
 function doStuff(value: any): void {
- if (typeof value === 'string') {
- console.log(value.toUpperCase().split('').join(' '));
- } else if (typeof value === 'number') {
- console.log(value.toPrecision(5));
- }
+    if (typeof value === 'string') {
+        console.log(value.toUpperCase().split('').join(' '));
+    } else if (typeof value === 'number') {
+        console.log(value.toPrecision(5));
+    }
 
- value; // coloque el cursor sobre `valor` aqui
- }
+    value; // coloque el cursor sobre `valor` aqui
+}
 
- doStuff(2);
- doStuff(22);
- doStuff(222);
- doStuff('hello');
- doStuff(true);
- doStuff({});
+doStuff(2);
+doStuff(22);
+doStuff(222);
+doStuff('hello');
+doStuff(true);
+doStuff({});
 
 console.log('[Ejercicio 4.1]');
--- Typescript_project/src/pruebas_teoria.ts
+++ Typescript_project/src/pruebas_teoria.ts
@@ -1,25 +1,24 @@
 function greet(greeting: string, name: string): string {
-return greeting + ' ' + name + '!';
+    return greeting + ' ' + name + '!';
 }
 greet('Hello', 'Steve');
 //greet('Hi');
 //greet('Hi', 'Bill', 'Gates');
 
 //Tipo Arreglos
-var jobs: Array<string> = [ 'IBM', 'Microsoft', 'Google' ];
-var jobs: string[] = ['Apple', 'Dell', 'HP' ];
+var jobs: Array<string> = ['IBM', 'Microsoft', 'Google'];
+var jobs: string[] = ['Apple', 'Dell', 'HP'];
 
 //Tipo void
 function sayHi(): void {
     console.log('Hi!');
-    }
+}
 
-
-    let speech: void = sayHi();
-    console.log(speech);
+let speech: void = sayHi();
+console.log(speech);
 
 //Tipo union
-let code: (string | number);
+let code: string | number;
 code = 123;
 code = 'ABC';
 //code = false;
