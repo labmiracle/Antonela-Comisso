21a7a0bed2d2da958c6f4c14fb9f9992f51f7948
hachecom <antocomisso@gmail.com>
2023-06-12 19:40:55 -0300

    Reorganización de carpetas

--- Practica_React/State/src/App.jsx
+++ Practica_React/State/src/App.jsx
@@ -4,7 +4,11 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  let [calc, setCalc] = useState({
+    sign: "",
+    num: 0,
+    res: 0,
+  });
   return (
     <>
       <div>
@@ -15,10 +19,36 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>+</button>
       </div>
       <div>
-        <a>count is {count}</a>
+        <a>CONTADOR: {count}</a>
       </div>
     </>
   );
 }
 
+function Screen({ value }) {
+  return <div className="screen">{value}</div>;
+}
+
+const Wrapper = ({ children }) => {
+  return <div className="wrapper">{children}</div>;
+};
+
+const ButtonBox = ({ children }) => {
+  return <div className="buttonBox">{children}</div>;
+};
+
+const Button = ({ className, value, onClick }) => {
+  return (
+    <button className={className} onClick={onClick}>
+      {value}
+    </button>
+  );
+};
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
 export default App;
