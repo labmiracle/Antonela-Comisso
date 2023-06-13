d38de4d4bbef4999820cde5f56013976d993226c
hachecom <antocomisso@gmail.com>
2023-06-13 14:17:53 -0300

    Intentando Ej N2 de State, nada importante

--- Practica_React/State/src/App.jsx
+++ Practica_React/State/src/App.jsx
@@ -4,11 +4,7 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  let [calc, setCalc] = useState({
-    sign: "",
-    num: 0,
-    res: 0,
-  });
+
   return (
     <>
       <div>
@@ -25,30 +21,4 @@ function App() {
   );
 }
 
-function Screen({ value }) {
-  return <div className="screen">{value}</div>;
-}
-
-const Wrapper = ({ children }) => {
-  return <div className="wrapper">{children}</div>;
-};
-
-const ButtonBox = ({ children }) => {
-  return <div className="buttonBox">{children}</div>;
-};
-
-const Button = ({ className, value, onClick }) => {
-  return (
-    <button className={className} onClick={onClick}>
-      {value}
-    </button>
-  );
-};
-const btnValues = [
-  ["C", "+-", "%", "/"],
-  [7, 8, 9, "X"],
-  [4, 5, 6, "-"],
-  [1, 2, 3, "+"],
-  [0, ".", "="],
-];
 export default App;
---
+++ Practica_React/State/src/App2.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.css";
+
+function App2() {
+  return (
+    <>
+      <p>Ejercicio N°2</p>
+    </>
+  );
+}
+export default App2;
