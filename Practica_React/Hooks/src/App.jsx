423a6774caf768cf2b7491277fb3b79e6a23dfc7
hachecom <antocomisso@gmail.com>
2023-06-21 18:42:54 -0300

    React:Hooks: Ejercicio N1

---
+++ Practica_React/Hooks/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [word, setWord] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+    const replaceWord = inputText.replaceAll("a", "b");
+    setWord(replaceWord);
+  };
+
+  return (
+    <>
+      <div>
+        <div>Ingrese texto:</div>
+        <input type="text" value={inputText} onChange={handleInputChange} />
+        <text>{word}</text>
+      </div>
+    </>
+  );
+}
+
+export default App;
