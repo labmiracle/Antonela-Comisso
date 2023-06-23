46f25d9be0cfd17d36aa8d529e45e1669e06b04d
hachecom <antocomisso@gmail.com>
2023-06-23 15:31:42 -0300

    React:Hooks: Ejercicio N°4

---
+++ Practica_React/Hooks/src/App4.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+const App4 = () => {
+  const [tareas, setTareas] = useState([]);
+
+  useEffect(() => {
+    const obtenerTareas = async () => {
+      try {
+        const respuesta = await fetch(
+          "https://jsonplaceholder.typicode.com/todos"
+        );
+        const datos = await respuesta.json();
+        setTareas(datos);
+      } catch (error) {
+        console.error("Error al obtener las tareas:", error);
+      }
+    };
+
+    obtenerTareas();
+  }, []);
+
+  return (
+    <div>
+      <h2>Tareas:</h2>
+      <ul className="pintar">
+        {tareas.map((tarea) => (
+          <li key={tarea.id}>{tarea.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App4;
--- Practica_React/Hooks/src/main.jsx
+++ Practica_React/Hooks/src/main.jsx
@@ -5,11 +5,13 @@ import App from "./App.jsx";
 import "./index.css";
 import App2 from "./App2.jsx";
 import App3 from "./App3.jsx";
+import App4 from "./App4.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <App2 />
     <App3 mensaje={"LUKE, I AM YOUR FATHER."} />
+    <App4 />
   </React.StrictMode>
 );
