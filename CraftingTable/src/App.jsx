fc588bf428396df4e3060b9bc90712e42a1830fe
hachecom <antocomisso@gmail.com>
2023-06-30 14:53:00 -0300

    Crafting Table segundo intento, ya puedo seleccionar y colocar items en el board. Correción de botón clear table

--- CraftingTable/src/App.jsx
+++ CraftingTable/src/App.jsx
@@ -2,30 +2,38 @@ import { useState } from "react";
 import spriteSheet from "./itemMC.png";
 import "./App.css";
 
-function Square({ value, onSquareClick }) {
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
-  );
-}
-
 export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [itemSelected, setItemSelected] = useState("HOLA");
+  //const [squares, setSquares] = useState(Array(9).fill(null));
+  const [itemSelected, setItemSelected] = useState();
+  const [xCoord, setXcoord] = useState(Array(9).fill(2));
+  const [yCoord, setYcoord] = useState(Array(9).fill(9));
+
+  function setArray(oldArray, positionInarray, replceFor) {
+    const newArray = [...oldArray];
+    newArray[positionInarray] = replceFor;
+    return newArray;
+  }
 
   function handleClick(i) {
-    const nextSquares = squares.slice();
+    let nextXcoord;
+    let nextYcoord;
+
     if (itemSelected === "COAL") {
-      nextSquares[i] = "C";
+      nextXcoord = 2;
+      nextYcoord = 4;
     }
+
     if (itemSelected === "ROCK") {
-      nextSquares[i] = "R";
+      nextXcoord = 1;
+      nextYcoord = 1;
     }
     if (itemSelected === "STICK") {
-      nextSquares[i] = "S";
+      nextXcoord = 1;
+      nextYcoord = 2;
     }
-    setSquares(nextSquares);
+
+    setXcoord(setArray(xCoord, i, nextXcoord));
+    setYcoord(setArray(yCoord, i, nextYcoord));
   }
 
   function handleCoalclick() {
@@ -43,6 +51,10 @@ export default function Board() {
     return;
   }
 
+  function setSquares(setX, setY) {
+    setXcoord(Array(9).fill(2));
+    setYcoord(Array(9).fill(9));
+  }
   return (
     <>
       <h2>Crafting Table</h2>
@@ -54,24 +66,59 @@ export default function Board() {
         Clear Table
       </button>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        <ItemMC
+          frameXcoordinate={xCoord[0]}
+          frameYcoordinate={yCoord[0]}
+          onSquareClick={() => handleClick(0)}
+        />
+        <ItemMC
+          frameXcoordinate={xCoord[1]}
+          frameYcoordinate={yCoord[1]}
+          onSquareClick={() => handleClick(1)}
+        />
+        <ItemMC
+          frameXcoordinate={xCoord[2]}
+          frameYcoordinate={yCoord[2]}
+          onSquareClick={() => handleClick(2)}
+        />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        <ItemMC
+          frameXcoordinate={xCoord[3]}
+          frameYcoordinate={yCoord[3]}
+          onSquareClick={() => handleClick(3)}
+        />
+        <ItemMC
+          frameXcoordinate={xCoord[4]}
+          frameYcoordinate={yCoord[4]}
+          onSquareClick={() => handleClick(4)}
+        />
+        <ItemMC
+          frameXcoordinate={xCoord[5]}
+          frameYcoordinate={yCoord[5]}
+          onSquareClick={() => handleClick(5)}
+        />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        <ItemMC
+          frameXcoordinate={xCoord[6]}
+          frameYcoordinate={yCoord[6]}
+          onSquareClick={() => handleClick(6)}
+        />
+        <ItemMC
+          frameXcoordinate={xCoord[7]}
+          frameYcoordinate={yCoord[7]}
+          onSquareClick={() => handleClick(7)}
+        />
+        <ItemMC
+          frameXcoordinate={xCoord[8]}
+          frameYcoordinate={yCoord[8]}
+          onSquareClick={() => handleClick(8)}
+        />
       </div>
       <div className="space"></div>
+      ---------------------------------------------------
       <h2>Inventory</h2>
-
       <div className="board-row">
         <ItemMineCraft
           frameXcoordinate={2}
@@ -79,7 +126,6 @@ export default function Board() {
           onMCClick={() => handleCoalclick()}
         />
       </div>
-
       <div className="board-row">
         <ItemMineCraft
           frameXcoordinate={1}
@@ -104,12 +150,13 @@ function ItemMineCraft({ frameXcoordinate, frameYcoordinate, onMCClick }) {
       <ItemMC
         frameXcoordinate={frameXcoordinate}
         frameYcoordinate={frameYcoordinate}
+        onSquareClick={onMCClick}
       />
     </button>
   );
 }
 
-function ItemMC({ frameXcoordinate, frameYcoordinate }) {
+function ItemMC({ frameXcoordinate, frameYcoordinate, onSquareClick }) {
   const frameWidth = 64;
   const frameHeight = 64;
 
@@ -122,30 +169,9 @@ function ItemMC({ frameXcoordinate, frameYcoordinate }) {
     height: frameHeight,
   };
 
-  return <div style={spriteStyle}></div>;
-}
-
-const Inventory = {
-  coal: [2, 4],
-  stick: [1, 2],
-};
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
+  return (
+    <button className="square" onClick={onSquareClick}>
+      <div style={spriteStyle}></div>
+    </button>
+  );
 }
