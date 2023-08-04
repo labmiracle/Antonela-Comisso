08e4eb12b6c5e9bd16f6943acd13d96a6f2f2fd8
hachecom <antocomisso@gmail.com>
2023-08-04 18:26:07 -0300

    fs modulo 4

--- Node-js/fs-modulo/app.js
+++ Node-js/fs-modulo/app.js
@@ -1,12 +1,23 @@
 const http = require("http");
 const fs = require("fs");
+const path = require("path");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
 
-  if (url === "/dirname") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(`El valor de __dirname es: ${__dirname}`);
+  if (url === "/about") {
+    const filePath = path.join(__dirname, "vistas", "about.html");
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/html" });
+        res.end(
+          "<html><body><h1>Error interno del servidor</h1></body></html>"
+        );
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
     res.end("<html><body><h1>404 Archivo no encontrado</h1></body></html>");
