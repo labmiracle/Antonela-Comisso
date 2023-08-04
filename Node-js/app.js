b4a319ceaa0b033820f2fb4fff8c151f23b44e6f
hachecom <antocomisso@gmail.com>
2023-08-04 15:42:48 -0300

    Node-js http module cambio en la respuesta

--- Node-js/app.js
+++ Node-js/app.js
@@ -1,13 +1,19 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+  const urlPart = req.url.substring(1);
   res.writeHead(200, { "Content-Type": "text/plain" });
+  const htmlResponse = `${urlPart}`;
+  //res.end(
+  //  "Hola este este es un servidor HTTP en Node.js de Anto Comisso para Miracle lab"
+  //);
+  res.end(htmlResponse);
 
-  res.end(
-    "Hola este es un servidor HTTP en Node.js hecho por Anto Comisso para MiracleLab!"
-  );
+  console.log("Solicitud recibida:");
+  console.log("Método:", req.method);
+  console.log("URL actual:", req.url);
+  console.log("Encabezados:", req.headers);
 });
-
 const port = 5000;
 
 server.listen(port, () => {
