707e9a1599e36123987e67f9855f4bc01213d633
hachecom <antocomisso@gmail.com>
2023-08-04 15:45:09 -0300

    Node-js http module enrutamiento simple

--- Node-js/app.js
+++ Node-js/app.js
@@ -2,19 +2,19 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   const urlPart = req.url.substring(1);
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  const htmlResponse = `${urlPart}`;
-  //res.end(
-  //  "Hola este este es un servidor HTTP en Node.js de Anto Comisso para Miracle lab"
-  //);
-  res.end(htmlResponse);
 
-  console.log("Solicitud recibida:");
-  console.log("Método:", req.method);
-  console.log("URL actual:", req.url);
-  console.log("Encabezados:", req.headers);
+  res.writeHead(200, { "Content-Type": "text/html" });
+
+  if (urlPart === "miraclelab") {
+    res.end("Bienvenido a MiracleLab");
+  } else if (urlPart === "about") {
+    res.end("Acerca de Nosotros");
+  } else {
+    res.end("Página no encontrada");
+  }
 });
-const port = 5000;
+
+const port = 3000;
 
 server.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
