adc64cd076a013351f5c67931d9738acd48b97d1
hachecom <antocomisso@gmail.com>
2023-08-04 18:29:19 -0300

    fs modulo 5

--- Node-js/fs-modulo/app.js
+++ Node-js/fs-modulo/app.js
@@ -10,17 +10,20 @@ const server = http.createServer((req, res) => {
     fs.readFile(filePath, "utf8", (err, data) => {
       if (err) {
         res.writeHead(500, { "Content-Type": "text/html" });
-        res.end(
+        res.write(
           "<html><body><h1>Error interno del servidor</h1></body></html>"
         );
+        res.end();
       } else {
         res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
+        res.write(data);
+        res.end();
       }
     });
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
-    res.end("<html><body><h1>404 Archivo no encontrado</h1></body></html>");
+    res.write("<html><body><h1>404 Archivo no encontrado</h1></body></html>");
+    res.end();
   }
 });
 
