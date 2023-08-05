848530df7e06861161d85de2afb1e494ce71c688
hachecom <antocomisso@gmail.com>
2023-08-05 09:20:40 -0300

    process 1y 2  terminado

---
+++ Node-js/fs-modulo/serverConfg.js
@@ -0,0 +1,12 @@
+const serverConfig = {
+  port: 3000,
+  host: "localhost",
+  database: {
+    host: "localhost",
+    username: "root",
+    password: "password",
+    database: "mydatabase",
+  },
+};
+
+module.exports = serverConfig;
